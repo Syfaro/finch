51f5a342eeabd4f367811c7d41145a57d3a46252
Syfaro <syfaro@foxpaw.in>
2015-12-31 14:39:16 -0600

    Add cancel command that hides a custom keyboard

---
+++ commands/cancel/cancel.go
@@ -0,0 +1,29 @@
+package finchcommandcancel
+
+import (
+	"github.com/syfaro/finch"
+	"github.com/syfaro/telegram-bot-api"
+)
+
+func init() {
+	finch.RegisterCommand(&cancelCommand{})
+}
+
+type cancelCommand struct {
+	finch.CommandBase
+}
+
+func (cmd *cancelCommand) ShouldExecute(update tgbotapi.Update) bool {
+	return finch.SimpleCommand("cancel", update.Message.Text)
+}
+
+func (cmd *cancelCommand) Execute(update tgbotapi.Update) error {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Your current command has been canceled.")
+	msg.ReplyToMessageID = update.Message.MessageID
+	msg.ReplyMarkup = tgbotapi.ReplyKeyboardHide{
+		HideKeyboard: true,
+		Selective:    true,
+	}
+
+	return cmd.Finch.SendMessage(msg)
+}
