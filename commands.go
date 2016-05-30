fccd5959e192ffe7a40266c7a0cb9672be6c29f4
Syfaro <syfaro@foxpaw.in>
2016-05-30 11:37:23 -0500

    Fix error for callback queries.

--- commands.go
+++ commands.go
@@ -87,7 +87,7 @@ func (f *Finch) commandRouter(update tgbotapi.Update) {
 	if update.CallbackQuery != nil {
 		for _, command := range f.Commands {
 			// check if the command is waiting for input
-			if command.IsWaiting(update.Message.From.ID) {
+			if command.IsWaiting(update.CallbackQuery.From.ID) {
 				if err := command.Command.ExecuteCallback(*update.CallbackQuery); err != nil {
 					f.commandError(command.Command.Help().Name, *update.CallbackQuery.Message, err)
 				}
